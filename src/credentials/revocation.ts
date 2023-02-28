5c865d824f1c9879b4282f470ffe2b45b1f3b4d0
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2023-02-28 15:04:13 +0200

    add Revocation Status DTO with proof unmarshaler

--- src/credentials/revocation.ts
+++ src/credentials/revocation.ts
@@ -15,7 +15,12 @@ import axios from 'axios';
 import { NODE_TYPE_LEAF, Proof } from '@iden3/js-merkletree';
 import { hashElems } from '@iden3/js-merkletree';
 import { DID } from '@iden3/js-iden3-core';
-import { CredentialStatus, RevocationStatus, RHSCredentialStatus } from '../verifiable';
+import {
+  CredentialStatus,
+  Issuer,
+  RevocationStatus,
+  RHSCredentialStatus
+} from '../verifiable';
 import { strMTHex } from '../circuits';
 
 /**
@@ -377,4 +382,55 @@ async function buildNodesUp(siblings: Siblings, node: NodeLeaf): Promise<ProofNo
   }
 
   return nodes;
+}
+/**
+ *  Proof dto as a partial result of fetching credential status with type SparseMerkleTreeProof
+ *
+ * @export
+ * @class ProofDTO
+ */
+export class ProofDTO {
+  existence: boolean;
+  siblings: string[];
+  node_aux: {
+    key: string;
+    value: string;
+  };
+}
+
+/**
+ * RevocationStatusDTO is a result of fetching credential status with type SparseMerkleTreeProof
+ * 
+ * @beta
+ * @export
+ * @class RevocationStatusDTO
+ */
+export class RevocationStatusDTO {
+  issuer: Issuer;
+  mtp: ProofDTO;
+
+  toRevocationStatus(): RevocationStatus {
+    const p = new Proof();
+    p.existence = this.mtp.existence;
+    p.nodeAux = this.mtp.node_aux ? {
+      key: newHashFromBigInt(BigInt(this.mtp.node_aux.key)),
+      value: newHashFromBigInt(BigInt(this.mtp.node_aux.value))
+    } as NodeAux : undefined;
+
+    const s = this.mtp.siblings.map((s) => newHashFromBigInt(BigInt(s)));
+
+    p.siblings = [];
+    p.depth = s.length;
+
+    for (let lvl = 0; lvl < s.length; lvl++) {
+      if (s[lvl].bigInt() !== BigInt(0)) {
+        setBitBigEndian(p.notEmpties, lvl);
+        p.siblings.push(s[lvl]);
+      }
+    }
+    return {
+      mtp: p,
+      issuer: this.issuer
+    };
+  }
 }
