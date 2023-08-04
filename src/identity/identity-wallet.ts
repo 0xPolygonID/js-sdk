2e81dbd68bfaac58e079c7ba943bd66355ed4524
vbasiuk <volodymyr.basiuck@gmail.com>
2023-08-04 16:13:01 +0300

    rm empty line

--- src/identity/identity-wallet.ts
+++ src/identity/identity-wallet.ts
@@ -311,6 +311,7 @@ export class IdentityWallet implements IIdentityWallet {
     await this._storage.mt.createIdentityMerkleTrees(tmpIdentifier);
 
     opts.seed = opts.seed ?? getRandomBytes(32);
+
     const keyId = await this._kms.createKeyFromSeed(KmsKeyType.BabyJubJub, opts.seed);
 
     const pubKeyHex = await this._kms.publicKey(keyId);
