4df45ee7eca4460d987588c74d34b4bf317f9d48
vbasiuk <volodymyr.basiuck@gmail.com>
2023-12-07 18:19:49 +0200

    remove linkNonce from query

--- src/proof/proof-service.ts
+++ src/proof/proof-service.ts
@@ -72,6 +72,7 @@ export interface ProofGenerationOptions {
   credential?: W3CCredential;
   verifierDID?: DID;
   authEnabled?: number;
+  linkNonce?: bigint;
 }
 
 export interface DIDProfileMetadata {
@@ -626,9 +627,7 @@ export class ProofService implements IProofService {
     circuitInputs.currentTimeStamp = getUnixTimestamp(new Date());
 
     circuitInputs.proofType = proofType;
-    circuitInputs.linkNonce = proofReq.query.linkNonce
-      ? BigInt(proofReq.query.linkNonce.toString())
-      : BigInt(0);
+    circuitInputs.linkNonce = params.linkNonce ?? BigInt(0);
     circuitInputs.verifierID = params.verifierDID ? DID.idFromDID(params.verifierDID) : undefined;
     circuitInputs.nullifierSessionID = proofReq.params?.nullifierSessionID
       ? BigInt(proofReq.params?.nullifierSessionID?.toString())
@@ -689,9 +688,7 @@ export class ProofService implements IProofService {
     circuitInputs.currentTimeStamp = getUnixTimestamp(new Date());
 
     circuitInputs.proofType = proofType;
-    circuitInputs.linkNonce = proofReq.query.linkNonce
-      ? BigInt(proofReq.query.linkNonce.toString())
-      : BigInt(0);
+    circuitInputs.linkNonce = params.linkNonce ?? BigInt(0);
     circuitInputs.verifierID = params.verifierDID ? DID.idFromDID(params.verifierDID) : undefined;
     circuitInputs.nullifierSessionID = proofReq.params?.nullifierSessionID
       ? BigInt(proofReq.params?.nullifierSessionID?.toString())
