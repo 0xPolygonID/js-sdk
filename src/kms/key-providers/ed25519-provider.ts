d5aa505ab4a3161b890f201f2544b78b317bb764
Alex Monso <amonsosanz@gmail.com>
2024-03-25 14:44:35 +0100

    Use @noble/curves instead of noble-ed25519

--- src/kms/key-providers/ed25519-provider.ts
+++ src/kms/key-providers/ed25519-provider.ts
@@ -1,12 +1,13 @@
 import { IKeyProvider } from '../kms';
 import { AbstractPrivateKeyStore, KmsKeyId, KmsKeyType } from '../store';
 import * as providerHelpers from '../provider-helpers';
-import { getPublicKey, sign, verify } from 'noble-ed25519';
+import { ed25519 } from '@noble/curves/ed25519';
+
 /**
  * Provider for Ed25519 keys
  * @public
  * @class Ed25519Provider
- * @implements implements IKeyProvider interface
+ * @implements IKeyProvider interface
  */
 export class Ed25519Provider implements IKeyProvider {
   /**
@@ -15,6 +16,7 @@ export class Ed25519Provider implements IKeyProvider {
    */
   keyType: KmsKeyType;
   private _keyStore: AbstractPrivateKeyStore;
+
   /**
    * Creates an instance of Ed25519Provider.
    * @param {KmsKeyType} keyType - kms key type
@@ -24,10 +26,11 @@ export class Ed25519Provider implements IKeyProvider {
     this.keyType = keyType;
     this._keyStore = keyStore;
   }
+
   /**
    * generates a ed25519 key from a seed phrase
    * @param {Uint8Array} seed - byte array seed
-   * @returns kms key identifier
+   * @returns {Promise<KmsKeyId>} kms key identifier
    */
   async newPrivateKeyFromSeed(seed: Uint8Array): Promise<KmsKeyId> {
     if (seed.length !== 32) {
@@ -35,11 +38,12 @@ export class Ed25519Provider implements IKeyProvider {
     }
 
     const privateKey = seed;
-    const publicKey = await getPublicKey(privateKey);
+    const publicKey = ed25519.getPublicKey(privateKey);
     const kmsId = {
       type: this.keyType,
       id: providerHelpers.keyPath(this.keyType, Buffer.from(publicKey).toString('hex'))
     };
+
     await this._keyStore.importKey({
       alias: kmsId.id,
       key: Buffer.from(privateKey).toString('hex')
@@ -50,33 +54,40 @@ export class Ed25519Provider implements IKeyProvider {
 
   /**
    * Gets public key by kmsKeyId
-   *
    * @param {KmsKeyId} keyId - key identifier
+   * @returns {Promise<string>} Public key as a hex string
    */
   async publicKey(keyId: KmsKeyId): Promise<string> {
     const privateKeyHex = await this.privateKey(keyId);
-    return getPublicKey(privateKeyHex);
+    const privateKey = Buffer.from(privateKeyHex, 'hex');
+    const publicKey = ed25519.getPublicKey(privateKey);
+    return Buffer.from(publicKey).toString('hex');
   }
 
   /**
    * signs prepared payload of size,
    * with a key id
-   *
    * @param {KmsKeyId} keyId  - key identifier
    * @param {Uint8Array} data - data to sign (32 bytes)
-   * @returns Uint8Array signature
+   * @returns {Promise<Uint8Array>} signature
    */
   async sign(keyId: KmsKeyId, data: Uint8Array): Promise<Uint8Array> {
     const privateKeyHex = await this.privateKey(keyId);
-    const publicKeyHex = await getPublicKey(privateKeyHex);
-    const signature = await sign(data, privateKeyHex);
-    const isValid = await verify(signature, data, publicKeyHex);
+    const privateKey = Buffer.from(privateKeyHex, 'hex');
+    const signature = await ed25519.sign(data, privateKey);
+    const publicKey = ed25519.getPublicKey(privateKey);
+    const isValid = await ed25519.verify(signature, data, publicKey);
     if (!isValid) {
       throw new Error('Signature is invalid');
     }
     return signature;
   }
 
+  /**
+   * Retrieves the private key for a given keyId from the key store.
+   * @param {KmsKeyId} keyId - The identifier of the key to retrieve.
+   * @returns {Promise<string>} The private key associated with the keyId.
+   */
   private async privateKey(keyId: KmsKeyId): Promise<string> {
     return this._keyStore.get({ alias: keyId.id });
   }
