1252b2e49046e046c04059345ad100ea0e270713
vbasiuk <volodymyr.basiuck@gmail.com>
2025-05-12 15:02:58 +0300

    add problem-report message type

--- src/iden3comm/constants.ts
+++ src/iden3comm/constants.ts
@@ -55,7 +55,9 @@ export const PROTOCOL_MESSAGE_TYPE = Object.freeze({
     `${DIDCOMM_PROTOCOL}discover-features/2.0/queries` as const,
   // DiscoveryProtocolDiscloseMessageType is type for didcomm discovery protocol disclose
   DISCOVERY_PROTOCOL_DISCLOSE_MESSAGE_TYPE:
-    `${DIDCOMM_PROTOCOL}discover-features/2.0/disclose` as const
+    `${DIDCOMM_PROTOCOL}discover-features/2.0/disclose` as const,
+  // ProblemReportMessageType is type for didcomm problem report
+  PROBLEM_REPORT_MESSAGE_TYPE: `${DIDCOMM_PROTOCOL}report-problem/2.0/problem-report` as const
 });
 
 /**
---
+++ src/iden3comm/types/protocol/problem-report.ts
@@ -0,0 +1,18 @@
+import { PROTOCOL_MESSAGE_TYPE } from '../../constants';
+import { BasicMessage } from '../packer';
+
+/** ProblemReportMessage is struct for problem report message */
+export type ProblemReportMessage = BasicMessage & {
+  body: ProblemReportMessageBody;
+  pthid: string;
+  ack?: string[];
+  type: typeof PROTOCOL_MESSAGE_TYPE.PROBLEM_REPORT_MESSAGE_TYPE;
+};
+
+/** ProblemReportMessageBody is struct for problem report message body */
+export type ProblemReportMessageBody = {
+  code: string;
+  comment?: string;
+  args?: string[];
+  escalate_to?: string;
+};
