31ef5c83551b56130b542e5f6fad51bcf1d72f78
Kolezhniuk <dmytro.k@privado.id>
2025-07-06 20:03:11 +0700

    Clean up

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,31 +1,10 @@
 import { defineConfig } from 'vitest/config';
-// import dotenv from 'dotenv';
 
 export default defineConfig({
   test: {
-    // Set environment variables directly
     globals: true,
     environment: 'node',
     testTimeout: 400000,
     setupFiles: ['dotenv/config']
-    // poolOptions: {
-    //   threads: {
-    //     maxThreads: 3
-    //   }
-    // }
-    // Allow files to run in parallel, but control test execution within files
-    // pool: 'threads',
-    // poolOptions: {
-    //   threads: {
-    //     singleThread: false
-    //   }
-    // },
-    // // Configure sequence behavior
-    // sequence: {
-    //   // Allow files to run concurrently (parallel)
-    //   concurrent: true,
-    //   // Don't shuffle test order
-    //   shuffle: false
-    // }
   }
 });
