fcdbd067f6b0127cc59f33b9aa8b2cffaeb29200
Kolezhniuk <dmytro.k@privado.id>
2025-07-08 19:13:33 +0700

    Increase timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    testTimeout: 60_000,
+    testTimeout: 120_000,
     setupFiles: ['dotenv/config']
   }
 });
