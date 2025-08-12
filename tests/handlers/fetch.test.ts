512dde4895103f1bb4b10a126c7f42219c90a4ca
Vladyslav Munin <vladcisco.vm@gmail.com>
2025-08-12 17:07:12 +0300

    remove only

--- tests/handlers/fetch.test.ts
+++ tests/handlers/fetch.test.ts
@@ -42,7 +42,7 @@ import { describe, expect, it, beforeEach, afterEach } from 'vitest';
 import path from 'path';
 import nock from 'nock';
 
-describe.only('fetch', () => {
+describe('fetch', () => {
   afterEach(() => {
     nock.cleanAll();
   });
@@ -121,6 +121,7 @@ describe.only('fetch', () => {
     "typ": "application/iden3comm-plain-json",
     "type": "https://iden3-communication.io/credentials/1.0/issuance-response"
 }`;
+
 
   beforeEach(async () => {
     const kms = registerKeyProvidersInMemoryKMS();
@@ -332,83 +333,9 @@ describe.only('fetch', () => {
     expect(response).to.not.be.undefined;
   });
 
-  it.only('issuance message handle generic', async () => {
-    const msg: CredentialIssuanceMessage = {
-      body: {
-        credential: W3CCredential.fromJSON({
-          '@context': [
-            'https://www.w3.org/2018/credentials/v1',
-            'https://schema.iden3.io/core/jsonld/iden3proofs.jsonld',
-            'ipfs://QmVoGggpzonebhQAXVdW5tjG5kYU9qSQiRKzt2BCRzcW3n'
-          ],
-          credentialSchema: {
-            id: 'ipfs://QmW6BqLb5E6NbKxUfarhwQtv2KNGAoqUHmAzJmWTW6WegU',
-            type: 'JsonSchema2023'
-          },
-          credentialStatus: {
-            id: 'https://issuer-node-core-api-testing.privado.id/v2/agent',
-            revocationNonce: 425820783,
-            type: CredentialStatusType.Iden3commRevocationStatusV1
-          },
-          credentialSubject: {
-            boolean: true,
-            'date-time': '2025-08-11T00:00:00.000+02:00',
-            id: 'did:iden3:billions:main:2VxLirxXFxi78GX62HujQuRAeUndfdJtUjqkFExcn7',
-            integer: 5,
-            'non-negative-integer': '2',
-            number: 4,
-            'positive-integer': '1',
-            'positive-integer-eth-address': '3',
-            string: "I'm mock refresh service",
-            type: 'PrimitiveAttributes'
-          },
-          expirationDate: '2025-08-11T21:13:10Z',
-          id: 'urn:uuid:9691b9f6-76f6-11f0-a048-0a58a9feac02',
-          issuanceDate: '2025-08-11T21:03:10.866282218Z',
-          issuer: 'did:iden3:polygon:amoy:xDyTLTEN36W2UTZExFcJho56xtMCMBLiX8emVABrb',
-          proof: [
-            {
-              coreClaim:
-                '34db46963b959d369b8886be6deb97f12a00000000000000000000000000000001b1f617fa280d2408a1aadd0e1bfe321e6eaecf08ab9f3370bf8baf3cce0c0099a40c01f1167e19bdc90f9bc87b99eb8e7fde50ddd434c29cbf7fdd8aec201900000000000000000000000000000000000000000000000000000000000000006f82611900000000e65c9a680000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-              issuerData: {
-                authCoreClaim:
-                  'cca3371a6cb1b715004407e325bd993c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044fe11077ff9d7451b583d95eee27bc6fa4071fd0db7d1da15df901106fa95205b3c59e80b687440dc3457eb8450142425b77a2fa7cba65d71235e108331f2040000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-                credentialStatus: {
-                  id: 'https://issuer-node-core-api-testing.privado.id/v2/agent',
-                  revocationNonce: 0,
-                  type: 'Iden3commRevocationStatusV1.0'
-                },
-                id: 'did:iden3:polygon:amoy:xDyTLTEN36W2UTZExFcJho56xtMCMBLiX8emVABrb',
-                mtp: {
-                  existence: true,
-                  siblings: []
-                },
-                state: {
-                  claimsTreeRoot:
-                    '3d68e778b762c5fb8562207f153d92b93f2c4843bc65d50cff9c9003d8e4de18',
-                  value: 'c4654e19579ab729733d390d85ee36b96929016ddac918a05301d69fb2a5e42f'
-                }
-              },
-              signature:
-                'bbb015278d38f7933392d83f6d0fb26f3e3ee7211e93aeccf0425b87ea72a78891fe405ba98d43887755d3646a7846beba6d917227420bd28d3082c4c2c91104',
-              type: 'BJJSignature2021'
-            }
-          ],
-          refreshService: {
-            id: 'https://refresh-service-demo.internal-iden3-dev.com',
-            type: 'Iden3RefreshService2023'
-          },
-          type: ['VerifiableCredential', 'PrimitiveAttributes']
-        })
-      },
-      from: 'did:iden3:polygon:amoy:xDyTLTEN36W2UTZExFcJho56xtMCMBLiX8emVABrb',
-      id: '22343ad4-6969-476e-8401-557e59db48c8',
-      thid: 'cb73ba0e-41e8-45e3-b5db-1ac7f6147c81',
-      to: 'did:iden3:billions:main:2VxLirxXFxi78GX62HujQuRAeUndfdJtUjqkFExcn7',
-      typ: PROTOCOL_CONSTANTS.MediaType.PlainMessage,
-      type: PROTOCOL_CONSTANTS.PROTOCOL_MESSAGE_TYPE.CREDENTIAL_ISSUANCE_RESPONSE_MESSAGE_TYPE
-    };
-
+  it('issuance message handle generic', async () => {
+    const msg: CredentialIssuanceMessage = JSON.parse(issuanceResponseMock);
+    msg.body.credential = W3CCredential.fromJSON(msg.body.credential); // that how it can be in the client.
     const response = await fetchHandler.handle(msg, {
       mediaType: PROTOCOL_CONSTANTS.MediaType.PlainMessage
     });
@@ -418,81 +345,8 @@ describe.only('fetch', () => {
     expect(cred).to.not.be.undefined;
   });
 
-  it.only('issuance message handleIssuance method', async () => {
-    const msg = `{
-    "body": {
-        "credential": {
-            "@context": [
-                "https://www.w3.org/2018/credentials/v1",
-                "https://schema.iden3.io/core/jsonld/iden3proofs.jsonld",
-                "ipfs://QmVoGggpzonebhQAXVdW5tjG5kYU9qSQiRKzt2BCRzcW3n"
-            ],
-            "credentialSchema": {
-                "id": "ipfs://QmW6BqLb5E6NbKxUfarhwQtv2KNGAoqUHmAzJmWTW6WegU",
-                "type": "JsonSchema2023"
-            },
-            "credentialStatus": {
-                "id": "https://issuer-node-core-api-testing.privado.id/v2/agent",
-                "revocationNonce": 425820783,
-                "type": "Iden3commRevocationStatusV1.0"
-            },
-            "credentialSubject": {
-                "boolean": true,
-                "date-time": "2025-08-11T00:00:00.000+02:00",
-                "id": "did:iden3:billions:main:2VxLirxXFxi78GX62HujQuRAeUndfdJtUjqkFExcn7",
-                "integer": 5,
-                "non-negative-integer": "2",
-                "number": 4,
-                "positive-integer": "1",
-                "positive-integer-eth-address": "3",
-                "string": "I'm mock refresh service",
-                "type": "PrimitiveAttributes"
-            },
-            "expirationDate": "2025-08-11T21:13:10Z",
-            "id": "urn:uuid:9691b9f6-76f6-11f0-a048-0a58a9feac02",
-            "issuanceDate": "2025-08-11T21:03:10.866282218Z",
-            "issuer": "did:iden3:polygon:amoy:xDyTLTEN36W2UTZExFcJho56xtMCMBLiX8emVABrb",
-            "proof": [
-                {
-                    "coreClaim": "34db46963b959d369b8886be6deb97f12a00000000000000000000000000000001b1f617fa280d2408a1aadd0e1bfe321e6eaecf08ab9f3370bf8baf3cce0c0099a40c01f1167e19bdc90f9bc87b99eb8e7fde50ddd434c29cbf7fdd8aec201900000000000000000000000000000000000000000000000000000000000000006f82611900000000e65c9a680000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-                    "issuerData": {
-                        "authCoreClaim": "cca3371a6cb1b715004407e325bd993c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044fe11077ff9d7451b583d95eee27bc6fa4071fd0db7d1da15df901106fa95205b3c59e80b687440dc3457eb8450142425b77a2fa7cba65d71235e108331f2040000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-                        "credentialStatus": {
-                            "id": "https://issuer-node-core-api-testing.privado.id/v2/agent",
-                            "revocationNonce": 0,
-                            "type": "Iden3commRevocationStatusV1.0"
-                        },
-                        "id": "did:iden3:polygon:amoy:xDyTLTEN36W2UTZExFcJho56xtMCMBLiX8emVABrb",
-                        "mtp": {
-                            "existence": true,
-                            "siblings": []
-                        },
-                        "state": {
-                            "claimsTreeRoot": "3d68e778b762c5fb8562207f153d92b93f2c4843bc65d50cff9c9003d8e4de18",
-                            "value": "c4654e19579ab729733d390d85ee36b96929016ddac918a05301d69fb2a5e42f"
-                        }
-                    },
-                    "signature": "bbb015278d38f7933392d83f6d0fb26f3e3ee7211e93aeccf0425b87ea72a78891fe405ba98d43887755d3646a7846beba6d917227420bd28d3082c4c2c91104",
-                    "type": "BJJSignature2021"
-                }
-            ],
-            "refreshService": {
-                "id": "https://refresh-service-demo.internal-iden3-dev.com",
-                "type": "Iden3RefreshService2023"
-            },
-            "type": [
-                "VerifiableCredential",
-                "PrimitiveAttributes"
-            ]
-        }
-    },
-    "from": "did:iden3:polygon:amoy:xDyTLTEN36W2UTZExFcJho56xtMCMBLiX8emVABrb",
-    "id": "22343ad4-6969-476e-8401-557e59db48c8",
-    "thid": "cb73ba0e-41e8-45e3-b5db-1ac7f6147c81",
-    "to": "did:iden3:billions:main:2VxLirxXFxi78GX62HujQuRAeUndfdJtUjqkFExcn7",
-    "typ": "application/iden3comm-plain-json",
-    "type": "https://iden3-communication.io/credentials/1.0/issuance-response"
-}`;
+  it('issuance message handleIssuance method', async () => {
+    const msg = issuanceResponseMock;
 
     const bts = await new PlainPacker().pack(byteEncoder.encode(msg));
     const response = await fetchHandler.handleIssuanceResponseMessage(bts);
