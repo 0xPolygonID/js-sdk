a152e15f9cd11dbb24dc39f680fb974a31b3c5b2
volodymyr-basiuk <31999965+volodymyr-basiuk@users.noreply.github.com>
2025-09-24 18:27:47 +0300

    make did doc builder to be exported (#344)

--- src/iden3comm/index.ts
+++ src/iden3comm/index.ts
@@ -2,8 +2,7 @@ export * from './packageManager';
 export * from './packers';
 export * from './types';
 export * from './handlers';
-export * from './utils/did';
-export * from './utils/accept-profile';
+export * from './utils';
 
 import * as PROTOCOL_CONSTANTS from './constants';
 export { PROTOCOL_CONSTANTS };
